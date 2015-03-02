a5a3e82413d7907146bfe6cc651209ca411567d2
HughesDaniel <danielh@toxicshock.biz>
2015-03-01 18:21:52 -0700

    no idea

--- bloctalk/src/main/java/com/bloc/bloctalk/api/BlocTalkSMSReceiver.java
+++ bloctalk/src/main/java/com/bloc/bloctalk/api/BlocTalkSMSReceiver.java
@@ -3,6 +3,11 @@ package com.bloc.bloctalk.api;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.telephony.SmsMessage;
+import android.util.Log;
+import android.widget.Toast;
 
 /**
  * BlocTalkSMSReceiver
@@ -16,9 +21,45 @@ import android.content.Intent;
  */
 public class BlocTalkSMSReceiver extends BroadcastReceiver {
 
+    private static final String TAG = ".BlocTalkSMSReciever.java";
+
+    private SmsManager sms = SmsManager.getDefault();
+
     @Override
     public void onReceive(Context context, Intent intent) {
-        // Handle an incoming SMS intent
-    }
 
+        // Retrieves a map of extended data from the intent.
+        final Bundle bundle = intent.getExtras();
+
+        try {
+
+            if (bundle != null) {
+
+                final Object[] pdusObj = (Object[]) bundle.get("pdus");
+
+                for (int i = 0; i < pdusObj.length; i++) {
+
+                    SmsMessage currentMessage = SmsMessage.createFromPdu((byte[]) pdusObj[i]);
+                    String phoneNumber = currentMessage.getDisplayOriginatingAddress();
+
+                    String senderNum = phoneNumber;
+                    String message = currentMessage.getDisplayMessageBody();
+
+                    Log.d(TAG, "senderNum: " + senderNum + "; message: " + message);
+
+
+                    // Show Alert
+                    int duration = Toast.LENGTH_LONG;
+                    Toast toast = Toast.makeText(context,
+                            "senderNum: "+ senderNum + ", message: " + message, duration);
+                    toast.show();
+
+                } // end for loop
+            } // bundle is null
+
+        } catch (Exception e) {
+            Log.e(TAG, "Exception smsReceiver" + e);
+
+        }
+    }
 }
